a7f9c2cc2b1cb10d9a678bb8368a27b2caab6a3f
ken <kenneth.ekpolugh@mymhc.ca>
2025-03-29 15:16:12 -0600

    Done

--- lht_calendar.js
+++ lht_calendar.js
@@ -31,7 +31,7 @@
 */
 
       /* Set the date displayed in the calendar */
-      var thisDay = new Date("September 24, 2021");
+      var thisDay = new Date();
       /* Write the calendar to the element with the id "calendar" */
       document.getElementById("calendar").innerHTML =
       createCalendar(thisDay);
@@ -111,11 +111,18 @@ if (thisYear % 4 === 0) {
  }
   // Write cells for each day of the month
    var totalDays = daysInMonth(calDate);
+
+   var highlightDay = calDate.getDate();
    for (var i = 1; i <= totalDays; i++) {
    day.setDate(i);
    weekDay = day.getDay();
+   
    if (weekDay === 0) htmlCode += "<tr>";
-   htmlCode += "<td class='calendar_dates'>" + i + "</td>";
+   if (i === highlightDay) {
+      htmlCode += "<td class='calendar_dates' id='calendar_today'>"+ i + dayEvent[i] + "</td>";
+     } else {
+      htmlCode += "<td class='calendar_dates'>" + i + dayEvent[i] + "</td>";
+     }
    if (weekDay === 6) htmlCode += "</tr>";
  }
    return htmlCode;
